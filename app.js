f7578565e4b252ccfbb84ae59083993e4dbd9ef0
jason <jason@weazm.com>
2014-05-05 20:41:59 +0800

    init

---
+++ app.js
@@ -0,0 +1,57 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var wechat = require('wechat');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.query());
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// app.get('/', routes.index);
+// app.get('/users', user.list);
+app.use('/wechat', wechat('some token').text(function (message, req, res, next) {
+  // TODO
+  console.log(message);
+  res.reply('text');
+}).image(function (message, req, res, next) {
+  // TODO
+}).voice(function (message, req, res, next) {
+  // TODO
+  console.log(message);
+  res.reply('text');
+}).video(function (message, req, res, next) {
+  // TODO
+}).location(function (message, req, res, next) {
+  // TODO
+}).link(function (message, req, res, next) {
+  // TODO
+}).event(function (message, req, res, next) {
+  // TODO
+}).middlewarify());
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
